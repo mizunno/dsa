1ca99dea46ce58b116b85db8d01c4cc08a93e823
Pedro <pedrogarciavictoria@gmail.com>
2023-06-20 21:22:15 +0200

    add optimizations to bubble sort

--- sorting.py
+++ sorting.py
@@ -1,11 +1,26 @@
 
 
 def bubble_sort(elements):
-    for i in range(1, len(elements)):
-        j = i        
-        while elements[j - 1] > elements[j] and j > 0:
-            elements[j - 1], elements[j] = elements[j], elements[j - 1]
-            j-=1
+    """
+    Sorts a list using the Bubble Sorting algorithm.
+    (https://www.programiz.com/dsa/bubble-sort)
+    """
+    for i in range(0, len(elements) - 1):
+
+        # track if swapping
+        swapped = False
+
+        for j in range(0, len(elements) - i - 1):
+
+            # if next element is lower than the current one, swap them
+            if elements[j + 1] < elements[j]:
+                elements[j + 1], elements[j] = elements[j], elements[j + 1]
+                swapped = True
+
+        # if no swap was made, the list is already sorted,
+        # so we avoid further comparison
+        if not swapped:
+            break
 
     return elements
 
