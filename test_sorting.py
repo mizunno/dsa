7c3ab900fc4c30bbe4fa7804f6a8a91985dadafe
Pedro <pedrogarciavictoria@gmail.com>
2023-06-24 20:43:24 +0200

    Refactor sorting tests

--- test_sorting.py
+++ test_sorting.py
@@ -1,24 +1,47 @@
 import unittest
-from sorting import bubble_sort
+import sorting
 
 
-class TestBubbleSort(unittest.TestCase):
+class TestSorting(unittest.TestCase):
+    def random_list_sorting_test(self, sorting_func, case):
+        with self.subTest(case=case + ' - Random List'):
+            input_list = [3, 1, 2, 5, 9, 7, 8, 6, 4]
+            expected_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            sorted_list = sorting_func(input_list)
+            self.assertListEqual(expected_list, sorted_list)
 
-    def test_random_list(self):
-        sorted_list = bubble_sort([3,1,2,5,9,7,8,6,4])
-        self.assertEqual([1,2,3,4,5,6,7,8,9], sorted_list)
 
-    def test_reversed_list(self):
-        sorted_list = bubble_sort([9,8,7,6,5,4,3,2,1])
-        self.assertEqual([1,2,3,4,5,6,7,8,9], sorted_list)
+    def reversed_list_sorting_test(self, sorting_func, case):
+        with self.subTest(case=case + ' - Reversed List'):
+            input_list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+            expected_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            sorted_list = sorting_func(input_list)
+            self.assertListEqual(expected_list, sorted_list)
 
-    def test_empty_list(self):
-        sorted_list = bubble_sort([])
-        self.assertEqual([], sorted_list)
 
-    def test_one_element_list(self):
-        sorted_list = [1]
-        self.assertEqual([1], sorted_list)
+    def empty_list_sorting_test(self, sorting_func, case):
+        with self.subTest(case=case + ' - Empty List'):
+            input_list = []
+            expected_list = []
+            sorted_list = sorting_func(input_list)
+            self.assertListEqual(expected_list, sorted_list)
+
+
+    def one_element_list_sorting_test(self, sorting_func, case):
+        with self.subTest(case=case + ' - One Element List'):
+            input_list = [1]
+            expected_list = [1]
+            sorted_list = sorting_func(input_list)
+            self.assertListEqual(expected_list, sorted_list)
+
+
+    def test_sorting_functions(self):
+        sorting_funcs = [(name,func) for name, func in sorting.__dict__.items() if callable(func)]
+        for func_name, sorting_func in sorting_funcs:
+            self.random_list_sorting_test(sorting_func, func_name)
+            self.reversed_list_sorting_test(sorting_func, func_name)
+            self.empty_list_sorting_test(sorting_func, func_name)
+            self.one_element_list_sorting_test(sorting_func, func_name)
 
 
 if __name__ == "__main__":
