1a04c535227c9113403bab114e0e11dd5b0af082
Pedro <pedrogarciavictoria@gmail.com>
2023-06-26 21:23:09 +0200

    Implement merge sort

--- sorting.py
+++ sorting.py
@@ -60,3 +60,54 @@ def selection_sort(elements):
         elements[smallest_idx], elements[i] = elements[i], elements[smallest_idx]
 
     return elements
+
+
+def merge_sort(elements):
+
+    """
+    Sort a list using the Merge Sort algorithm. It is
+    a Divide and Conquer algorithm.
+    - Divide: divides the list into sublists.
+    - Conquer: merges sublists while sorting the elements.
+    (https://www.programiz.com/dsa/merge-sort)
+    """
+
+    # Base case: return the input if the list has one element
+    if len(elements) <= 1:
+        return elements
+    
+    # Divide the list into two sublists
+    mid = len(elements) // 2
+    left, right = elements[:mid], elements[mid:]
+
+    # Recursive calls to continue dividing the list until reaching the 
+    # base case
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+
+    # Merge phase: merge both lists into one final list while sorting 
+    # their elements
+    left_idx, right_idx = 0, 0
+    merged = []
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            merged.append(left[left_idx])
+            left_idx+=1
+        else:
+            merged.append(right[right_idx])
+            right_idx+=1
+
+    
+    # Append remaining elements from the left sublist
+    while left_idx < len(left):
+        merged.append(left[left_idx])
+        left_idx+=1;
+
+    # Append remaining elements from the right sublist
+    while right_idx < len(right):
+        merged.append(right[right_idx])
+        right_idx+=1;
+
+    return merged
