89fa0d3c01cd3022bf33efdd790936defce93db8
Pedro <pedrogarciavictoria@gmail.com>
2023-07-09 23:53:43 +0200

    bst boilerplate

---
+++ bst.py
@@ -0,0 +1,33 @@
+
+
+class BST:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+    def insert(self, value):
+        pass
+
+    def min(self, value):
+        pass
+
+    def max(self, value):
+        pass
+
+    def delete(self, value):
+        pass
+
+    def preorder(self):
+        pass
+
+    def postorder(self):
+        pass
+
+    def inorder(self):
+        pass
+
+    def __repr__(self) -> str:
+        pass
---
+++ test_bst.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class TestBST(unittest.TestCase):
+
+    def test_insert(self):
+        pass
+
+    def test_min(self):
+        pass
+
+    def test_max(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def preorder(self):
+        pass
+
+    def postorder(self):
+        pass
+
+    def inorder(self):
+        pass
