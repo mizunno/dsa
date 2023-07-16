4fc3da11903f1c9fb3e32914450d8c69da7e8675
Pedro <pedrogarciavictoria@gmail.com>
2023-07-16 17:55:40 +0200

    add delete method and unit tests

--- bst.py
+++ bst.py
@@ -21,11 +21,13 @@ class BST:
             else:
                 self.right.insert(value)
 
+
     def min(self):
         if not self.left:
             return self.value
         
         return self.left.min()
+
 
     def max(self):
         if not self.right:
@@ -33,17 +35,52 @@ class BST:
         
         return self.right.max()
 
+
     def delete(self, value):
-        pass
+        if not self.value:
+            return
+        
+        if value < self.value:
+            if self.left:
+                self.left = self.left.delete(value)
+            
+            return self
+        
+        elif value > self.value:
+            if self.right:
+                self.right = self.right.delete(value)
+
+            return self
+            
+        else:
+            if self.left is None:
+                return self.right
+            
+            if self.right is None:
+                return self.left
+            
+            min_greater_node = self.right
+            while min_greater_node.left:
+                min_greater_node = min_greater_node.left
+
+            self.value = min_greater_node.value
+            self.right = self.right.delete(min_greater_node.value)
+
+            return self
+
+
 
     def preorder(self):
         pass
 
+
     def postorder(self):
         pass
+
 
     def inorder(self):
         pass
 
+
     def __repr__(self) -> str:
-        pass
+        return str(self.value)
--- test_bst.py
+++ test_bst.py
@@ -4,9 +4,11 @@ from bst import BST
 
 class TestBST(unittest.TestCase):
 
+
     def setUp(self) -> None:
         self.bst = BST(10)
         return super().setUp()
+    
 
     def test_insert(self):
         self.bst.insert(5)
@@ -19,6 +21,7 @@ class TestBST(unittest.TestCase):
         self.assertEqual(self.bst.left.left.value, 2)
         self.assertEqual(self.bst.left.right.value, 8)
         self.assertEqual(self.bst.right.value, 15)
+
 
     def test_min_max(self):
         self.assertEqual(self.bst.min(), 10)
@@ -34,7 +37,8 @@ class TestBST(unittest.TestCase):
         self.assertEqual(self.bst.min(), 5)
         self.assertEqual(self.bst.max(), 15)
 
-    def test_delete(self):
+
+    def test_delete_leaf_node(self):
         """
             -> 15
         10 
@@ -55,8 +59,8 @@ class TestBST(unittest.TestCase):
         self.assertEqual(self.bst.left.value, 5)
         self.assertEqual(self.bst.left.left.value, 2)
         self.assertEqual(self.bst.left.right.value, 8)
-        self.assertEqual(self.bst.left.right.left, 6)
-        self.assertEqual(self.bst.left.right.right, 9)
+        self.assertEqual(self.bst.left.right.left.value, 6)
+        self.assertEqual(self.bst.left.right.right.value, 9)
         self.assertEqual(self.bst.right.value, 15)
 
         self.bst.delete(2)
@@ -65,18 +69,77 @@ class TestBST(unittest.TestCase):
         self.assertEqual(self.bst.left.value, 5)
         self.assertIsNone(self.bst.left.left)
         self.assertEqual(self.bst.left.right.value, 8)
-        self.assertEqual(self.bst.left.right.left, 6)
-        self.assertEqual(self.bst.left.right.right, 9)
+        self.assertEqual(self.bst.left.right.left.value, 6)
+        self.assertEqual(self.bst.left.right.right.value, 9)
+        self.assertEqual(self.bst.right.value, 15)
+
+
+    def test_delete_inner_node_bypass(self):
+
+        """
+            -> 15
+        10 
+                    -> 9
+                -> 8
+                    -> 6
+            -> 5
+        """
+        self.bst.insert(5)
+        self.bst.insert(15)
+        self.bst.insert(8)
+        self.bst.insert(6)
+        self.bst.insert(9)
+
+        self.assertEqual(self.bst.value, 10)
+        self.assertEqual(self.bst.left.value, 5)
+        self.assertEqual(self.bst.left.right.value, 8)
+        self.assertEqual(self.bst.left.right.left.value, 6)
+        self.assertEqual(self.bst.left.right.right.value, 9)
+        self.assertEqual(self.bst.right.value, 15)
+
+        self.bst.delete(5)
+
+        self.assertEqual(self.bst.value, 10)
+        self.assertEqual(self.bst.left.value, 8)
+        self.assertEqual(self.bst.left.left.value, 6)
+        self.assertEqual(self.bst.left.right.value, 9)
+        self.assertEqual(self.bst.right.value, 15)
+
+
+    def test_delete_inner_node_not_bypass(self):
+
+        """
+            -> 15
+        10 
+                    -> 9
+                -> 8
+                    -> 6
+            -> 5
+                -> 2
+        """
+        self.bst.insert(5)
+        self.bst.insert(15)
+        self.bst.insert(2)
+        self.bst.insert(8)
+        self.bst.insert(6)
+        self.bst.insert(9)
+
+        self.assertEqual(self.bst.value, 10)
+        self.assertEqual(self.bst.left.value, 5)
+        self.assertEqual(self.bst.left.left.value, 2)
+        self.assertEqual(self.bst.left.right.value, 8)
+        self.assertEqual(self.bst.left.right.left.value, 6)
+        self.assertEqual(self.bst.left.right.right.value, 9)
         self.assertEqual(self.bst.right.value, 15)
 
         self.bst.delete(5)
 
         self.assertEqual(self.bst.value, 10)
         self.assertEqual(self.bst.left.value, 6)
-        self.assertIsNone(self.bst.left.left)
+        self.assertEqual(self.bst.left.left.value, 2)
         self.assertEqual(self.bst.left.right.value, 8)
         self.assertIsNone(self.bst.left.right.left)
-        self.assertEqual(self.bst.left.right.right, 9)
+        self.assertEqual(self.bst.left.right.right.value, 9)
         self.assertEqual(self.bst.right.value, 15)
 
 
