1fab32b7af1e02844037a237f33bb911e891c24b
Pedro <pedrogarciavictoria@gmail.com>
2023-07-17 21:34:49 +0200

    add stack and unit tests

---
+++ test_stack.py
@@ -0,0 +1,62 @@
+import unittest
+import stack
+
+class TestStack(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.stack = stack.Stack()
+        return super().setUp()
+    
+    
+    def test_push(self):
+        self.stack.push(1)
+        self.assertListEqual(self.stack.items, [1])
+
+        self.stack.push(2)
+        self.assertListEqual(self.stack.items, [1,2])
+
+
+    def test_pop(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertListEqual(self.stack.items, [1,2,3])
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertListEqual(self.stack.items, [1,2])
+
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertListEqual(self.stack.items, [1])
+        
+        self.assertEqual(self.stack.pop(), 1)
+        self.assertListEqual(self.stack.items, [])
+
+
+    def test_peek(self):
+        self.stack.push(1)
+        self.stack.push(2)
+
+        self.assertListEqual(self.stack.items, [1,2])
+
+        self.assertEqual(self.stack.peek(), 2)
+        self.assertListEqual(self.stack.items, [1,2])
+        
+
+    def test_len(self):
+        self.stack.push(1)
+        self.assertEqual(len(self.stack), 1)
+
+        self.stack.push(2)
+        self.assertEqual(len(self.stack), 2)
+
+        self.stack.push(3)
+        self.assertEqual(len(self.stack), 3)
+
+        self.stack.pop()
+        self.assertEqual(len(self.stack), 2)
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
